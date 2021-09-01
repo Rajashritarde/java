057584ff040bb40c42e3f685bf36c8dc68de0825
Rajashritarde <87028341+Rajashritarde@users.noreply.github.com>
2021-09-01 10:37:18 +0530

    Create celis.java

---
+++ celis.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+
+	public class celis  
+	{  
+	  public static void main (String args[])  import java.util.Scanner;
+
+	public class celis  
+	{  
+	  public static void main (String args[])  
+	   {
+			Scanner sc = new Scanner(System.in);
+		  float Fahrenheit, Celsius ;  
+	   System.out.println("Enter temperature in Celsius: ");
+	   Celsius = sc.nextFloat();
+	      
+	         
+	         Fahrenheit =((Celsius*9)/5)+32;  
+	         System.out.println("Temperature in Fahrenheit is: "+Fahrenheit);  
+	  
+	   sc.close();
+	   
+	   }}  
+
+
+	   {
+			Scanner sc = new Scanner(System.in);
+		  float Fahrenheit, Celsius ;  
+	   System.out.println("Enter temperature in Celsius: ");
+	   Celsius = sc.nextFloat();
+	      
+	         
+	         Fahrenheit =((Celsius*9)/5)+32;  
+	         System.out.println("Temperature in Fahrenheit is: "+Fahrenheit);  
+	  
+	   sc.close();
+	   
+	   }}  
+
